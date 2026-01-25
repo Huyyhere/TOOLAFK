5ec221cc9ed7fa764f181ee1e346525db6e8957a
Huyyhere <bquangghuyy2011@gmail.com>
2026-01-25 11:30:12 +0700

    Update SwitchKaitun.lua

--- SwitchKaitun.lua
+++ SwitchKaitun.lua
@@ -1,1 +1,126 @@
+-- Blox Fruits Kaitun Script - Full Config
 
+_G.Quest = {
+    ["RGB Aura Haki"] = true,
+    ["Pull Lever"] = true,
+    ["Quest Dough Awaken"] = true
+}
+
+_G.Race = {
+    ["Select Race"] = {"Human","Fishman","Skypiea","Mink","Ghoul","Cyborg"},
+    ["Lock Race"] = true,
+    ["Evo Race V2"] = true,
+    ["Evo Race V3"] = true,
+    ["Evo Race V4"] = true
+}
+
+_G.Melee = {
+    ["Combat"] = true,
+    ["Dark Step"] = true,
+    ["Electro"] = true,
+    ["Fishman Karate"] = true,
+    ["Dragon Claw"] = true,
+    ["Superhuman"] = true,
+    ["Death Step"] = true,
+    ["Sharkman Karate"] = true,
+    ["Electric Claw"] = true,
+    ["Dragon Talon"] = true,
+    ["Godhuman"] = true,
+    ["Sanguine Art"] = true
+}
+
+_G.Sword = {
+    ["Katana"] = true,
+    ["Cutlass"] = true,
+    ["Dual Katana"] = true,
+    ["Iron Mace"] = true,
+    ["Triple Katana"] = true,
+    ["Pipe"] = true,
+    ["Bisento"] = true,
+    ["Dual-Headed Blade"] = true,
+    ["Soul Cane"] = true,
+    ["Saber"] = true,
+    ["Midnight Blade"] = true,
+    ["Shisui"] = true,
+    ["Saddi"] = true,
+    ["Wando"] = true,
+    ["Yama"] = true,
+    ["Koko"] = true,
+    ["Rengoku"] = true,
+    ["Canvander"] = true,
+    ["Buddy Sword"] = true,
+    ["Twin Hooks"] = true,
+    ["SpikeyTrident"] = true,
+    ["Hallow Scryte"] = true,
+    ["Dark Dagger"] = true,
+    ["Tushita"] = true,
+    ["True Triple Katana"] = true,
+    ["Cursed Dual Katana"] = true,
+    ["Shark Anchor"] = true,
+    ["Pole V1"] = true,
+    ["Pole V2"] = true,
+    ["Dragon Trident"] = true,
+    ["Gravity Cane"] = true,
+    ["Fox Lamp"] = true,
+    ["Trident"] = true
+}
+
+_G.Gun = {
+    ["Slingshot"] = true,
+    ["Musket"] = true,
+    ["Flintlock"] = true,
+    ["Refined Slingshot"] = true,
+    ["Refined Flintlock"] = true,
+    ["Cannon"] = true,
+    ["Kabucha"] = true,
+    ["Bizarre Rifle"] = true,
+    ["Acidum Rifle"] = true,
+    ["Soul Guitar"] = true,
+    ["Serpent Bow"] = true
+}
+
+_G.Fruit = {
+    ["Main Fruit"] = {"T-Rex-T-Rex","Kitsune-Kitsune","Tiger-Tiger","Yeti-Yeti","Control-Control","Gas-Gas","Venom-Venom","Gravity-Gravity"},
+    ["Select Fruit"] = {
+        "Flame-Flame",
+        "Ice-Ice",
+        "Sand-Sand",
+        "Dark-Dark",
+        "Light-Light",
+        "Magma-Magma",
+        "Quake-Quake",
+        "Buddha-Buddha",
+        "Spider-Spider",
+        "Phoenix-Phoenix",
+        "Dough-Dough",
+        "Lightning-Lightning"
+    },
+    ["Auto Store Fruit"] = true,
+    ["Auto Awaken"] = true
+}
+
+_G.Mastery = {
+    ["Melee"] = true,
+    ["Sword"] = true,
+    ["Fruit"] = true,
+    ["Gun"] = true
+}
+
+_G.BossFarm = {
+    ["Auto Farm Boss"] = true,
+    ["Dough King"] = true,
+    ["Cake Prince"] = true,
+    ["rip_indra"] = true
+}
+
+_G.Setting = {
+    ["FPS Booster"] = true,
+    ["Safe Tween"] = true,
+    ["Speed Tween"] = 300,
+    ["Fast Attack"] = true,
+    ["Bring Mob"] = true,
+    ["Auto Haki"] = true
+}
+
+getgenv().Deleted_Ui = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/q8ta0e/roblox/main/SwitchHubKaitun.lua"))()
